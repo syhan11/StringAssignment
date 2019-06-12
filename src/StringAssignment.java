9df4a01dbe3d083e23b4477907b3dfe26c477c91
syhan11 <sueyoung.6311@gmail.com>
2019-06-12 11:02:43 -0400

    First commit

---
+++ src/StringAssignment.java
@@ -0,0 +1,11 @@
+/*
+Write a Java program that assigns your name to a variable and prints the variable to the console.
+ */
+
+public class StringAssignment {
+    public static void main(String[] arg){
+        String userName = "Sue Han";
+
+        System.out.println("My name is " + userName + ".");
+    }
+}
